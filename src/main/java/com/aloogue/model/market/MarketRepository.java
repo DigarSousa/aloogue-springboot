afb293cd73e98e3039a6ac13ba47eb8a85f35b32
edgar <digarsg@gmail.com>
2016-01-03 23:16:59 -0200

    Start repositories JPA

---
+++ src/main/java/com/aloogue/model/market/MarketRepository.java
@@ -0,0 +1,7 @@
+package com.aloogue.model.market;
+
+/**
+ * Created by edgar on 03/01/16.
+ */
+public class MarketRepository {
+}
---
+++ src/main/java/com/aloogue/model/user/User.java
@@ -0,0 +1,7 @@
+package com.aloogue.model.user;
+
+/**
+ * Created by edgar on 03/01/16.
+ */
+public class User {
+}
